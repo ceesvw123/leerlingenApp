d7ef44b23271d6659633e5c73dc562f9e7bcd9cf
ceesvw123 <ceesvanwanrooy@OSX1010.local>
2016-02-16 14:56:26 -0300

    app123
    123

---
+++ app/app.js
@@ -0,0 +1,13 @@
+/*global App (for now) */
+
+var app = app || {};
+
+function initApp()
+{
+	
+	// dit is het startpunt van je applicatie. Hier geef je aan de view(s) door welke models ze moeten gebruiken
+	
+}
+
+// als de pagina geladen is, dan starten we de app
+window.addEventListener("load", initApp);
---
+++ app/models/StudentsModel.js
@@ -0,0 +1,5 @@
+var app = app || {};
+
+// maak hier een app.StudentsModel object
+// dit object bevat de functies om studenten op te halen, etc.
+// vanuit dit bestand ga je ook je PHP API lastig vallen met de vraag "mag ik alle studenten"
